bc600c6d105f0d00c6e231d5477f378c8aeb4d38
Justinguu <justinguuu@gmail.com>
2023-03-20 14:03:37 -0400

    GA

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,48 +1,22 @@
 import Theme from '../styles/theme';
-
+import GoogleAnalytics from '@bradgarropy/next-google-analytics'
 import { useRouter } from 'next/router';
-import { useEffect } from "react";
-import * as gtag from "../../lib/gtag"
-import Script from "next/script"
 
-// import the Script
+
+
 
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
-//uses the useRouter and useEffect hooks to record a page view every time the user navigates to another page.
-  useEffect(() => {
-      const handleRouteChange = (url) => {
-      gtag.pageview(url);
-      };
-    
-      router.events.on("routeChangeComplete", handleRouteChange);
-    
-        return () => {
-        router.events.off("routeChangeComplete", handleRouteChange);
-        };
-      }, [router.events]);
+
     
   return (
     <>
   
-      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-V4WB28TEJC" />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-V4WB28TEJC', {
-            page_path: window.location.pathname,
-          });
-        `,
-        }}
-      />
+    
    
       <Theme>
+        <GoogleAnalytics measurementId="G-V4WB28TEJC" />
         <Component {...pageProps} />
       </Theme>
     </>
