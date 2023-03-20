1ab36b903fd514679bf036555aa82e2e61793d90
Justinguu <justinguuu@gmail.com>
2023-03-20 12:57:15 -0400

    GA working?

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,45 @@
 import Theme from '../styles/theme';
-import ReactGA from 'react-ga';
 
-const TRACKING_ID = "G-VKKBKTH192"
-ReactGA.initialize(TRACKING_ID);
+import { useRouter } from 'next/router';
+import { useEffect } from "react";
+import * as gtag from "../../lib/gtag"
+import Head from "next/head"
 
+// import Script from 'next/script'
 export default function App({ Component, pageProps }) {
-
+  const router = useRouter();
+//uses the useRouter and useEffect hooks to record a page view every time the user navigates to another page.
+  useEffect(() => {
+      const handleRouteChange = (url) => {
+      gtag.pageview(url);
+      };
+    
+      router.events.on("routeChangeComplete", handleRouteChange);
+    
+        return () => {
+        router.events.off("routeChangeComplete", handleRouteChange);
+        };
+      }, [router.events]);
+    
   return (
     <>
+    <Head>
+      <script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-V4WB28TEJC" />
+      <script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-V4WB28TEJC', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
+      </Head>
       <Theme>
         <Component {...pageProps} />
       </Theme>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,47 +1,29 @@
-import Acomplishments from '../components/Acomplishments/Acomplishments';
-import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation';
-import Hero from '../components/Hero/Hero';
-import Projects from '../components/Projects/Projects';
-import Technologies from '../components/Technologies/Technologies';
-import Timeline from '../components/TimeLine/TimeLine';
-import { Layout } from '../layout/Layout';
-import { Section } from '../styles/GlobalComponents';
+import Acomplishments from "../components/Acomplishments/Acomplishments";
+import BgAnimation from "../components/BackgrooundAnimation/BackgroundAnimation";
+import Hero from "../components/Hero/Hero";
+import Projects from "../components/Projects/Projects";
+import Technologies from "../components/Technologies/Technologies";
+import Timeline from "../components/TimeLine/TimeLine";
+import { Layout } from "../layout/Layout";
+import { Section } from "../styles/GlobalComponents";
 
-import Head from 'next/head';
 
 const Home = () => {
-
   return (
-      <>
-<Head>
-<script async src={`https://www.googletagmanager.com/gtag/js?id=G-V4WB28TEJC`}
-/>
 
-<script
-  dangerouslySetInnerHTML={{
-    __html: `
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', G-V4WB28TEJC}', {
-      page_path: window.location.pathname,
-    })
-    `
-  }}
-/>
-</Head>
+    
 
-    <Layout>
-      <Section grid>
-        <Hero />
-        <BgAnimation />
-      </Section>
-      <Projects />
-      <Technologies />
-      <Timeline />
-      <Acomplishments />
-    </Layout>
-</>
+      <Layout>
+        <Section grid>
+          <Hero />
+          <BgAnimation />
+        </Section>
+        <Projects />
+        <Technologies />
+        <Timeline />
+        <Acomplishments />
+      </Layout>
+
   );
 };
 export default Home;
