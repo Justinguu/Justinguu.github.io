ad412fb1a45940fa5583189a6810f0e10a8f7fa4
Justinguu <justinguuu@gmail.com>
2023-11-13 12:56:45 -0500

    erased pic

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -3,7 +3,7 @@ import React, { useState, useRef, useEffect } from 'react';
 import { CarouselButton, CarouselButtonDot, CarouselButtons, CarouselContainer, CarouselItem, CarouselItemImg, CarouselItemText, CarouselItemTitle, CarouselMobileScrollNode } from './TimeLineStyles';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { TimeLineData } from '../../constants/constants';
-// import profilePic from '../../../public/images/profile.jpeg';
+
 //
 const TOTAL_CAROUSEL_COUNT = TimeLineData.length;
 
